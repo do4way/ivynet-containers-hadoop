6843e3b4292e030bfc9198b9174f81e1b055c36f
Yongwei Dou <yongwei.dou@gmail.com>
2018-10-01 15:17:21 +0900

    Updated hadoop to v2.8.5.

--- datanode/run.sh
+++ datanode/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
-if [ ! -d $datadir ]; then
-    echo "Datanode data directory not found: $datadir"
-    exit 2
-fi
+strs=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
+datadirs=`echo $strs | tr "," "\n"`
+for datadir in $datadirs
+do
+    echo $datadir
+    if [ ! -d $datadir ]; then
+        echo "Datanode data directory not found: $datadir"
+        exit 2
+    fi
+done
 
 $HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR datanode
